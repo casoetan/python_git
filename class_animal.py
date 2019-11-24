9144aa5defe90ad77291c91e5baf34fd10b98624
Raheem Abioye <raheem.abioye@yahoo.ca>
2019-11-24 14:32:04 -0500

    class

--- class_animal.py
+++ class_animal.py
@@ -4,12 +4,8 @@ def __init__(self):
 def num_legs(self):
     return 2
 
-def talk(self):
-    return ''
-
 def color(self):
     return 'black'
 
-class monkey(animal):
-    def talk(self):
-        return 'screech'
+def talk(self):
+    return 'screech'
